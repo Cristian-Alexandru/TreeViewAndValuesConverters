7dbc29257c00373829210dbb163f9f32f5ea877d
Cristian-Alexandru <33779914+Cristian-Alexandru@users.noreply.github.com>
2020-12-14 20:00:09 +0200

    Update

---
+++ HeaderToImageConverter.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeViewAndValuesConverters
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of a drive, folder, file
+    /// </summary>
+ 
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Get the full path
+            var path = (string)value;
+
+            //If the path is null
+            if (path == null)
+                return null;
+            var name = MainWindow.GetFileFolderName(path);
+
+            //By default, we presume an image
+            var image = "Images/file.png";
+
+            //If the name is blank, we presume it's drive as we cannot have a blank file or folder
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TreeViewAndValuesConverters
 {
@@ -20,9 +10,178 @@ namespace TreeViewAndValuesConverters
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region DefaultConstructor
+        /// <summary>
+        /// DefaultConstructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        #endregion DefaultConstructor
+
+        #region On Load
+        /// <summary>
+        /// What the application do when is opened
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Get every logicla driver on machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //Create a new item for it
+                var items = new TreeViewItem()
+                {
+                    //Seat the header
+                    Header = drive,
+                    //Add the full path
+                    Tag = drive
+                };
+
+                //Add a dumy items
+                items.Items.Add(null);
+
+                //Listen out for items being expanded
+                items.Expanded += Folder_Expanded;
+
+                //Add it to the main tree view
+                FolderView.Items.Add(items);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// When a folder is expanded, find the sub folder/files
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            #region Initial Checks
+            var item = (TreeViewItem)sender;
+
+            //If the item only contains the dumy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            //Clear the items off
+            item.Items.Clear();
+
+            //Get full path
+            var fullPath = (string)item.Tag;
+            #endregion
+
+            #region Folder Expande
+            //Create a blank list for directories
+            var directories = new List<string>();
+
+            //Trying to get directories from the folder
+            //Ignoring any issues
+            try
+            {
+                var dir = Directory.GetDirectories(fullPath);
+                if (dir.Length > 0)
+                    directories.AddRange(dir);
+
+            }
+            catch { }
+
+            directories.ForEach(directoryPath =>
+            {
+                //Create directory item
+                var subItem = new TreeViewItem()
+                {
+                    //Set header as folder name
+                    //In this situation header represent the name of the folder
+                    //Ex: C:
+                    Header = GetFileFolderName(directoryPath),
+                    //Set tag as folder path
+                    //In this situation tag represent the full path of the folder
+                    //Ex: C:/ProgramFile/Google/Chrome/chrome.exe
+                    Tag = directoryPath
+                };
+
+                //Adding a dumy item for expanding the folder
+                subItem.Items.Add(null);
+
+                //Handle the expanding out
+                subItem.Expanded += Folder_Expanded;
+                //Add this items to the parrent
+                item.Items.Add(subItem);
+
+            });
+            #endregion
+
+            #region Get File
+            //Create a blank list for directories
+            var files = new List<string>();
+
+            //Trying to get file from the folder
+            //Ignoring any issues
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+                if (fs.Length > 0)
+                    files.AddRange(fs);
+
+            }
+            catch { }
+
+            files.ForEach(filePath =>
+            {
+                //Create file item
+                var subItem = new TreeViewItem()
+                {
+                    //Set header as file name
+                    //In this situation header represent the name of the folder
+                    //Ex: C:
+                    Header = GetFileFolderName(filePath),
+                    //Set tag as folder path
+                    //In this situation tag represent the full path of the folder
+                    //Ex: C:/ProgramFile/Google/Chrome/chrome.exe
+                    Tag = filePath
+                };
+                //Add this items to the parrent
+                item.Items.Add(subItem);
+
+            });
+            #endregion
+
+
+        }
+
+
+
+        #region Helper
+        /// <summary>
+        /// Return the file or folder name from a full path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            //C:\xxx\xxx\xxx\file.png
+
+            //If we have a null  or empty string return empty
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            //Make all slashes, back slashes
+            var normalizePath = path.Replace('/', '\\');
+            //Find the last backslash
+            var lastIndex = normalizePath.LastIndexOf('\\');
+
+            //If we don't find a \ return the path itself
+            if (lastIndex <= 0)
+                return path;
+
+            //Return name after the last \
+            return path.Substring(lastIndex + 1);
+            #endregion
+
         }
     }
 }
